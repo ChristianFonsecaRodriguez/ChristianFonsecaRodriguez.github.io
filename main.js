1e657efc4f47d842ca9d738e916c62685ee4bfb3
ChristianFonsecaRodriguez <cfonsecar@uni.pe>
2023-03-16 05:50:21 +0000

    second-attempt

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = window.location.pathname.split('/').pop();
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     const navLinks = document.querySelectorAll('#nav-bar a');
 
     navLinks.forEach(link => {
