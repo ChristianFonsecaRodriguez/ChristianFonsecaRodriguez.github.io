aa3a49dcd5bd99e2bc2d1b5c868f9e947edff4c2
ChristianFonsecaRodriguez <cfonsecar@uni.pe>
2023-03-29 03:56:59 -0500

    btn certification chaged to bootstrap type button ans added ligh and dark mode

--- resume.js
+++ resume.js
@@ -3,6 +3,7 @@ const switchBtn = document.getElementById('switch');
 const body = document.body;
 const righColumn = document.getElementById('rightColumn');
 const hrTitled = document.getElementById('hrTitled');
+const btnCert = document.getElementById('btnCert');
 
 
 const leftColumn = document.getElementById('leftColumn');
@@ -16,12 +17,14 @@ switchBtn.addEventListener('change', function() {
     leftColumn.classList.add('dark-mode');
     righColumn.classList.add('dark-mode');
     hrTitled.classList.add('dark-mode');
+    btnCert.classList.replace("btn-secondary", "btn-primary");
     label.innerHTML = 'Dark Mode';
   } else {
     body.classList.remove('dark-mode');
     leftColumn.classList.remove('dark-mode');
     righColumn.classList.remove('dark-mode');
     hrTitled.classList.remove('dark-mode');
+    btnCert.classList.replace("btn-primary", "btn-secondary");
     label.innerHTML = 'Light Mode';
   }
 });
